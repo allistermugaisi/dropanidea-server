1ab9c532977a2bc7f32290ec97a3de437cb0aff5
allistermugaisi <allistermugaisi@gmail.com>
2022-01-13 12:55:35 +0300

    fixed cors origin

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ mongoose
 
 app.use(express.json()); // used to parse JSON bodies
 app.use(express.urlencoded({ limit: '30mb', extended: true })); // parse URL-encoded bodies
-app.use(cors());
+app.use(cors('*'));
 app.use(morgan('common'));
 
 // Routes middleware
