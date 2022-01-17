72b706912a34913dd07fcc8caae25b2260b627c6
allistermugaisi <allistermugaisi@gmail.com>
2022-01-17 18:15:35 +0300

    added psychometric test functionality

--- controllers/psychometricTest.js
+++ controllers/psychometricTest.js
@@ -1,6 +1,24 @@
 import User from '../models/Users.js';
 
 export const psychometricTest = async (req, res) => {
-	const { label, description, options } = req.body;
-	console.log(req.body);
+	try {
+		let userId = req.userId;
+		let answeredQuiz = {
+			question: req.body.question,
+			answer: req.body.answer,
+			timestamp: new Date(),
+		};
+
+		await User.findOneAndUpdate(
+			{
+				_id: userId, // find user by id
+			},
+			{ $push: { psychometricTest: answeredQuiz } } // push test question to User model by id
+		);
+
+		res.status(200).json({ message: 'New question added' });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error });
+	}
 };
