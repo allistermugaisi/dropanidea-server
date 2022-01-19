79972ce0436058f179cbd25ea29b8fe21e931bc3
allistermugaisi <allistermugaisi@gmail.com>
2022-01-19 20:35:44 +0300

    created a Global level idea functionality

--- controllers/idea.js
+++ controllers/idea.js
@@ -13,24 +13,51 @@ export const createIdea = async (req, res) => {
 
 		const currentUser = await User.findById(userId);
 
-		// Create an idea
-		const createdIdea = await Ideas.create({
-			title,
-			description,
-			level: currentUser.role,
-			conceptualist: userId,
-			isIdeaActive: true,
-		});
+		// getMartha user id
+		// level = global
 
-		// appendUserIdeaArray
-		await User.findOneAndUpdate(
-			{
-				_id: createdIdea.conceptualist._id, // find user by id
-			},
-			{ $push: { ideas: createdIdea._id } } // push idea array to User model by id
-		);
+		if (
+			currentUser._id == '61e1059af5aab1f746e513b0' ||
+			currentUser._id == '61e0fe34f5aab1f746e513a3'
+		) {
+			// Create an Global idea
+			const createdIdea = await Ideas.create({
+				title,
+				description,
+				level: 'Global',
+				conceptualist: userId,
+				isIdeaActive: true,
+			});
 
-		res.status(200).json({ message: 'New idea created!' });
+			// appendUserIdeaArray
+			await User.findOneAndUpdate(
+				{
+					_id: createdIdea.conceptualist._id, // find user by id
+				},
+				{ $push: { ideas: createdIdea._id } } // push idea array to User model by id
+			);
+
+			res.status(200).json({ message: 'New idea created!' });
+		} else {
+			// Create an idea
+			const createdIdea = await Ideas.create({
+				title,
+				description,
+				level: currentUser.role,
+				conceptualist: userId,
+				isIdeaActive: true,
+			});
+
+			// appendUserIdeaArray
+			await User.findOneAndUpdate(
+				{
+					_id: createdIdea.conceptualist._id, // find user by id
+				},
+				{ $push: { ideas: createdIdea._id } } // push idea array to User model by id
+			);
+
+			res.status(200).json({ message: 'New idea created!' });
+		}
 	} catch (error) {
 		console.log(error);
 		res.status(500).json({ message: error });
@@ -41,7 +68,9 @@ export const getIdeas = async (req, res) => {
 	try {
 		let currentUser = await User.findById(req.userId);
 		// Ideas retrieved based on User role
-		let getIdeas = await Ideas.find({ level: currentUser.role })
+		let getIdeas = await Ideas.find({
+			$or: [{ level: currentUser.role }, { level: 'Global' }],
+		})
 			.populate('conceptualist')
 			.populate('discussions');
 		res.status(200).json(getIdeas);
